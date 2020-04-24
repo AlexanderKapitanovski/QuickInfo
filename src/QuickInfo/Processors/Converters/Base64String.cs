40e6dc1a5914a1d6c8eb71b6dde647465492300b
Alexander Kapitanovski <git@kapitanovski.de>
2020-04-24 20:40:37 +0200

    base64 string decode

---
+++ src/QuickInfo/Processors/Converters/Base64String.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using static QuickInfo.NodeFactory;
+
+namespace QuickInfo.Processors.Converters
+{
+    class Base64String : IProcessor
+    {
+        public object GetResult(Query query)
+        {
+            string base64String = query.OriginalInput;
+            byte[] data = null;
+            if (string.IsNullOrEmpty(base64String) || base64String.Length % 4 != 0
+               || base64String.Contains(" ") || base64String.Contains("\t") || base64String.Contains("\r") || base64String.Contains("\n"))
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(base64String, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None))
+            {
+                return false;
+            }    
+
+            try
+            {
+                data = System.Convert.FromBase64String(base64String);                            
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            if (data != null)
+            {
+                var pairs = new List<(string, string)>
+                {
+                ("UTF8:",  Encoding.UTF8.GetString(data)),
+                ("Unicode:",  Encoding.Unicode.GetString(data)),
+                ("ASCI:", Encoding.ASCII.GetString(data)),
+                ("Byte[]:", BitConverter.ToString(data))
+                };
+                return NameValueTable(null, right => right.Style = "Fixed", pairs.ToArray());
+            }
+            return null;
+        }
+
+    }
+}
